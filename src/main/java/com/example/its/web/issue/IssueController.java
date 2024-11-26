587c79fedd48e5cc49f21bfbc5cbc5f73b8bd398
yohei.hokari <yohei.hokari@gmail.com>
2024-11-26 23:47:24 +0900

    first commit

---
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -0,0 +1,22 @@
+package com.example.its.web.issue;
+
+import org.springframework.ui.Model;
+import com.example.its.domain.issue.IssueEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class IssueController {
+    @GetMapping("/issues")
+    public String showList(Model model) {
+        var issueList = List.of(
+                new IssueEntity(1, "概要1", "説明1"),
+                new IssueEntity(2, "概要2", "説明2"),
+                new IssueEntity(3, "概要3", "説明3")
+        );
+        model.addAttribute("IssueList", issueList);
+        return "issues/list";
+    }
+}
