af141276c454d0107d3a78d1b86aaa41c4d63af2
yohei.hokari <yohei.hokari@gmail.com>
2024-12-01 21:03:13 +0900

    課題作成と詳細画面の実装

--- src/main/java/com/example/its/domain/issue/IssueService.java
+++ src/main/java/com/example/its/domain/issue/IssueService.java
@@ -1,16 +1,26 @@
 package com.example.its.domain.issue;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class IssueService {
+    final IssueRepository issueRepository;
     public List<IssueEntity> findAll() {
-        return List.of(
-                new IssueEntity(1, "概要1", "説明1"),
-                new IssueEntity(2, "概要2", "説明2"),
-                new IssueEntity(3, "概要3", "説明3")
-        );
+        return issueRepository.findAll();
+    }
+
+    @Transactional
+    public void create(String summary, String description) {
+        issueRepository.insert(summary, description);
+    }
+
+    public IssueEntity findById(long issueId) {
+        return issueRepository.findById(issueId);
     }
 }
--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -5,17 +5,40 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import com.example.its.domain.issue.IssueEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RequiredArgsConstructor
+@RequestMapping("/issues")
 @Controller
 public class IssueController {
     private final IssueService issueService;
 
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model) {
         model.addAttribute("IssueList", issueService.findAll());
         return "issues/list";
+    }
+
+    @GetMapping("/creationForm")
+    public String showCreationForm(@ModelAttribute IssueForm form) {
+        return "issues/creationForm";
+    }
+
+    @PostMapping
+    public String create(@Validated IssueForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+          return showCreationForm(form);
+        }
+        issueService.create(form.getSummary(), form.getDescription());
+        return "redirect:/issues";
+    }
+
+    @GetMapping("/{issueId}")
+    public String showDetail(@PathVariable("issueId") long issueId, Model model) {
+        model.addAttribute("issue", issueService.findById(issueId));
+        return "issues/detail";
     }
 }
