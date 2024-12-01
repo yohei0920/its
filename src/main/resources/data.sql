95cee9ef41a401358b1f32d7e8391a30047064f0
yohei.hokari <yohei.hokari@gmail.com>
2024-12-01 19:31:50 +0900

    データベースの作成と初期データの投入

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into issues (summary, description) values ('バグA', '一覧にバグがあります');
+insert into issues (summary, description) values ('バグB', '詳細にバグがあります');
+insert into issues (summary, description) values ('バグC', 'DBにバグがあります');
