f9be6f05af1841d7bf92c943402d6192f42a56a0
yohei.hokari <yohei.hokari@gmail.com>
2024-12-01 19:13:35 +0900

    add: lombok、h2database、mybatisのセットアップ

---
+++ src/main/java/com/example/its/domain/issue/IssueService.java
@@ -0,0 +1,16 @@
+package com.example.its.domain.issue;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IssueService {
+    public List<IssueEntity> findAll() {
+        return List.of(
+                new IssueEntity(1, "概要1", "説明1"),
+                new IssueEntity(2, "概要2", "説明2"),
+                new IssueEntity(3, "概要3", "説明3")
+        );
+    }
+}
--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -1,22 +1,21 @@
 package com.example.its.web.issue;
 
+import com.example.its.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import com.example.its.domain.issue.IssueEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @Controller
 public class IssueController {
+    private final IssueService issueService;
+
     @GetMapping("/issues")
     public String showList(Model model) {
-        var issueList = List.of(
-                new IssueEntity(1, "概要1", "説明1"),
-                new IssueEntity(2, "概要2", "説明2"),
-                new IssueEntity(3, "概要3", "説明3")
-        );
-        model.addAttribute("IssueList", issueList);
+        model.addAttribute("IssueList", issueService.findAll());
         return "issues/list";
     }
 }
